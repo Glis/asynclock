fe597698e84fe4b4db004aebd935bc7c07c10291
Jorge Fuentes <glisterkaiser@gmail.com>
2019-02-24 13:18:07 -0400

    Version 1 ready. Using API+redis per-request
    When a request to the home is made, the REDIS store is asked for the coordinates, then, the API is requested sequentially to obtain the data, and it's showed on the view.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,31 @@
 class HomeController < ApplicationController
   def index
+    api_key = Rails.application.credentials.forecast_api_key
     cities = JSON.parse($redis.get('place_coordinates'))
-    @city_information = {}
+
+    # Place Information Hash
+    @places = {}
     cities.each do |name, coordinates|
-      @city_information[name] ||= {}
-      @city_information[name]['latitude'] = coordinates.split(',').first
-      @city_information[name]['longitude'] = coordinates.split(',').last
+      # rest-client requests
+      url = "https://api.darksky.net/forecast/#{api_key}/#{coordinates}"
+      result = JSON.parse(RestClient.get(url))
+
+      @places[name] ||= {}
+      @places[name]['latitude'] = result['latitude']
+      @places[name]['longitude'] = result['longitude']
+      @places[name]['timezone_name'] = result['timezone']
+      @places[name]['currently'] = result['currently']
+      @places[name]['unixtime'] = result['currently']['time']
+      @places[name]['localtime'] = TZInfo::Timezone.get(@places[name]['timezone_name']).utc_to_local(Time.at(@places[name]['unixtime']))
+      @places[name]['localtime_formatted'] = @places[name]['localtime'].strftime("%I:%M %p")
+      @places[name]['temperature_f'] = result['currently']['temperature']
+      @places[name]['temperature_c'] = sprintf('%.2f', fahrenheith_to_celsius(result['currently']['temperature']))
     end
+  end
+
+  private
+
+  def fahrenheith_to_celsius fahrenheit
+    (fahrenheit - 32) * 5 / 9
   end
 end
