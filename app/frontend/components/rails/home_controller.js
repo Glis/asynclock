bc3be59f856eb5d6f0a136da4fa38a2af315a9c4
Jorge Fuentes <glisterkaiser@gmail.com>
2019-02-26 02:07:23 -0400

    Testing a clock process in JS

--- app/frontend/components/rails/home_controller.js
+++ app/frontend/components/rails/home_controller.js
@@ -11,6 +11,23 @@ import { Controller } from "stimulus"
 
 export default class extends Controller {
   connect() {
-    console.log('connect home')
+    console.log('connect home');
+
+    this.initPolling();
+  }
+
+  initPolling() {
+    // Initial Request
+    this.requestCount = this.count(0);
+
+    // Set clock process to poll for data every 10 secs
+    setInterval(() => {
+      this.requestCount = this.count(this.requestCount);
+    }, 10000);
+  }
+
+  count(counter){
+    console.log(`Updating view. Request #${counter}`);
+    return counter+1;
   }
 }
