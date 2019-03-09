23e74069d5d2a5c1cd3e3cee99cd00f8f78f3466
Jorge Fuentes <glisterkaiser@gmail.com>
2019-03-09 05:13:41 +0000

    [HOTFIX] Using plain env to configure herokuredis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,5 @@
-$redis = Redis::Namespace.new("asynclock_#{Rails.env}", redis: Redis.new(url: ENV.fetch("REDIS_URL") { "redis://localhost:6379" }))
+if Rails.env.development? || Rails.env.test?
+  $redis = Redis::Namespace.new("asynclock_#{Rails.env}", redis: Redis.new(url: ENV.fetch("REDIS_URL") { "redis://localhost:6379" }))
+else
+  $redis = Redis::Namespace.new("asynclock_#{Rails.env}", redis: Redis.new(url: ENV["REDIS_URL"]))
+end
