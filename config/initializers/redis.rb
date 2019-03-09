80d62978bf9b8a6879f0c1593f9a1bc0fdc319f7
Jorge Fuentes <glisterkaiser@gmail.com>
2019-03-09 04:35:09 +0000

    [HOTFIX] RedisToGo was full and stopped working. migrating to HerokuRedis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,1 @@
-if Rails.env.development? || Rails.env.test?
-  $redis = Redis::Namespace.new("asynclock_#{Rails.env}", redis: Redis.new(url: ENV.fetch("REDIS_URL") { "redis://localhost:6379" }))
-else
-  $redis = Redis::Namespace.new("asynclock_#{Rails.env}", redis: Redis.new(url: ENV.fetch("REDISTOGO_URL") { "redis://localhost:6379" }))
-end
+$redis = Redis::Namespace.new("asynclock_#{Rails.env}", redis: Redis.new(url: ENV.fetch("REDIS_URL") { "redis://localhost:6379" }))
